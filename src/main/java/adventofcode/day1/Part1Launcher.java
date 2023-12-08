b215c8c5789388d40c4c7ed946c921645dead846
Changhoon Han <shirohoo4102@gmail.com>
2023-12-09 01:31:01 +0900

    Part1 refactored

--- src/main/java/adventofcode/day1/Part1Launcher.java
+++ src/main/java/adventofcode/day1/Part1Launcher.java
@@ -4,9 +4,9 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
-public class Launcher {
+public class Part1Launcher {
   public static void main(String[] args) {
-    Part1 part1 = new Part1();
+    var part1 = new Part1();
 
     try (var lines = Files.lines(Path.of("src/main/resources/day1/input.txt"))) {
       System.out.println(lines.mapToInt(part1::solve).sum());
